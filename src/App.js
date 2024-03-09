77528d00d0772f037bde6363213c05d85b23f781
José <joseluisfc2002@gmail.com>
2024-03-09 21:05:05 +0000

    refactor(repo): added pretttier and comitizen to the repo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Route ,Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Error from './components/Error';
-import Recover from './components/Recover';
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Error from './components/Error'
+import Recover from './components/Recover'
 
 const App = () => {
   return (
     <div>
-        <Routes>
-            <Route path= '/' element={<Login />} />
-            <Route path= '/finance_app' element={<Login />} />
-            <Route path= '/login' element={<Login />} />
-            <Route path= '/Error' element={<Error />} />
-            <Route path= '/Signup' element={<Signup />} />
-            <Route path= '/Recover' element={<Recover />}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/finance_app" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Error" element={<Error />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Recover" element={<Recover />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Recover.jsx
+++ src/components/Recover.jsx
@@ -1,34 +1,24 @@
-import React, {useRef, useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-
-
 export default function Recover() {
-
-    
- 
   return (
     <>
-    
-    <div class="main-container">
+      <div class="main-container">
         <div class="form-container">
           <h1>Recover Account</h1>
-          
-          <form >
+
+          <form>
             <div>
-              <input type="text" placeholder="Enter email"  required></input>
+              <input type="text" placeholder="Enter email" required></input>
             </div>
-            
 
-            <Link to={"/Login"}>
-              <button type="submit">
-                Submit
-              </button>
+            <Link to={'/Login'}>
+              <button type="submit">Submit</button>
             </Link>
-
           </form>
         </div>
-    </div>
+      </div>
     </>
   )
 }
